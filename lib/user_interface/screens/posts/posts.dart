1ec62642c7ee6096b85b2641ce97ef1120bec0dd
Daniel Yoon <danielyoonx3@gmail.com>
2023-07-15 03:39:43 -0400

    Made layout for post

--- lib/user_interface/screens/posts/posts.dart
+++ lib/user_interface/screens/posts/posts.dart
@@ -1,15 +1,123 @@
+import 'package:flutter/gestures.dart';
 import 'package:sangjishik/core_packages.dart';
 
 class Posts extends StatefulWidget {
-  const Posts({super.key});
+  final String image;
+  final String title;
+  final DateTime date;
+  final List<String> topics;
+
+  const Posts({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.date,
+    required this.topics,
+  });
 
   @override
   State<Posts> createState() => _PostsState();
 }
 
 class _PostsState extends State<Posts> {
+  List<Widget> topicWidgets = [];
+
+  bool isHover = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    topicWidgets =
+        widget.topics.map((String topic) => TopicBox(topic: topic)).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('POSTS!');
+    return MouseRegion(
+      onEnter: (PointerEnterEvent event) => setState(() {
+        isHover = true;
+      }),
+      onExit: (PointerExitEvent event) => setState(() {
+        isHover = false;
+      }),
+      child: GestureDetector(
+        onTap: () => print('TEST'),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular($styles.insets.xs),
+            color: isHover ? Colors.grey.withOpacity(.2) : Colors.transparent,
+          ),
+          child: SizedBox(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all($styles.insets.sm),
+                    child: LimitedBox(
+                      maxHeight: 300,
+                      child: AspectRatio(
+                        aspectRatio: 16 / 9,
+                        child: Image.network(
+                          widget.image,
+                          fit: BoxFit.fitWidth,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: $styles.insets.sm),
+                  child: Text(widget.title, style: $styles.text.bodyBold),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: $styles.insets.sm),
+                  child:
+                      Text(widget.date.toString(), style: $styles.text.caption),
+                ),
+                VSpace.sm,
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: $styles.insets.sm),
+                  child: Row(
+                    children: topicWidgets,
+                  ),
+                ),
+                VSpace.sm,
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TopicBox extends StatelessWidget {
+  final String topic;
+
+  const TopicBox({super.key, required this.topic});
+
+  @override
+  Widget build(BuildContext context) {
+    MaterialColor getColor(String topic) {
+      return Colors.grey;
+    }
+
+    return Padding(
+      padding: EdgeInsets.only(right: $styles.insets.sm),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25),
+          color: Colors.pink,
+        ),
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.all($styles.insets.xxs),
+            child: Text(topic),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/user_interface/screens/posts/posts_screen.dart
+++ lib/user_interface/screens/posts/posts_screen.dart
@@ -1,5 +1,6 @@
 import 'package:sangjishik/core_packages.dart';
 import 'package:sangjishik/user_interface/screens/posts/date_scrollbar.dart';
+import 'package:sangjishik/user_interface/screens/posts/posts.dart';
 
 class PostsScreen extends StatefulWidget {
   const PostsScreen({super.key});
@@ -11,8 +12,20 @@ class PostsScreen extends StatefulWidget {
 class _PostsScreenState extends State<PostsScreen> {
   @override
   Widget build(BuildContext context) {
-    double height = context.heightPx;
+    int calculateNumberOfPosts(int screenWidth) {
+      if (screenWidth >= 1200) {
+        return 4; // Display 4 posts for larger screens
+      } else if (screenWidth >= 900) {
+        return 3; // Display 3 posts for medium-sized screens
+      } else if (screenWidth >= 600) {
+        return 2; // Display 2 posts for smaller screens
+      } else {
+        return 1; // Display 1 post for the smallest screens
+      }
+    }
 
+    double height = context.heightPx;
+    double width = context.widthPx;
     return Expanded(
       child: Padding(
         padding: EdgeInsets.only(left: $styles.insets.lg),
@@ -26,11 +39,72 @@ class _PostsScreenState extends State<PostsScreen> {
                   children: [
                     Expanded(
                       child: GridView.count(
-                        crossAxisCount: 3,
+                        crossAxisCount: calculateNumberOfPosts(width as int),
                         mainAxisSpacing: 20,
                         crossAxisSpacing: 20,
-                        childAspectRatio: 1.3,
-                        children: [],
+                        childAspectRatio: 1,
+                        children: [
+                          Posts(
+                            image:
+                                'https://1409791524.rsc.cdn77.org/data/images/full/585357/kim-chaewon.jpg',
+                            title: 'TEST',
+                            date: DateTime.now(),
+                            topics: [
+                              'Personal',
+                              'Chaewon',
+                            ],
+                          ),
+                          Posts(
+                            image:
+                                'https://1409791524.rsc.cdn77.org/data/images/full/585357/kim-chaewon.jpg',
+                            title: 'TEST',
+                            date: DateTime.now(),
+                            topics: [
+                              'Personal',
+                              'Chaewon',
+                            ],
+                          ),
+                          Posts(
+                            image:
+                                'https://1409791524.rsc.cdn77.org/data/images/full/585357/kim-chaewon.jpg',
+                            title: 'TEST',
+                            date: DateTime.now(),
+                            topics: [
+                              'Personal',
+                              'Chaewon',
+                            ],
+                          ),
+                          Posts(
+                            image:
+                                'https://1409791524.rsc.cdn77.org/data/images/full/585357/kim-chaewon.jpg',
+                            title: 'TEST',
+                            date: DateTime.now(),
+                            topics: [
+                              'Personal',
+                              'Chaewon',
+                            ],
+                          ),
+                          Posts(
+                            image:
+                                'https://1409791524.rsc.cdn77.org/data/images/full/585357/kim-chaewon.jpg',
+                            title: 'TEST',
+                            date: DateTime.now(),
+                            topics: [
+                              'Personal',
+                              'Chaewon',
+                            ],
+                          ),
+                          Posts(
+                            image:
+                                'https://1409791524.rsc.cdn77.org/data/images/full/585357/kim-chaewon.jpg',
+                            title: 'TEST',
+                            date: DateTime.now(),
+                            topics: [
+                              'Personal',
+                              'Chaewon',
+                            ],
+                          ),
+                        ],
                       ),
                     ),
                     VSpace.xl,
